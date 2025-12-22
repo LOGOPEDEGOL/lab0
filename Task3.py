5439e54ba4a01eef4922dde21f41b093e25e0004
LOGOPEDEGOL <iegoradamoff@yandex.ru>
2025-12-22 11:50:24 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,15 +1,22 @@
-month,day  = map(int, input("Введите дату (месяц и день через пробел): ").split())
+inpt  = input("Введите дату (месяц и день через пробел): ").split()
 year_time = ["Зима","Весна","Лето","Осень"]
-def is_date_correct(day, month):
-
+def is_date_correct(inpt):
     output = []
     max_day_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-    if not (1 <= month <= 12):
-        output.append("Месяц должен быть от 1 до 12.")
+    if len(inpt) != 2:
+        output.append("Представьте дату в формате месяц <пробел> день")
     else:
-        if not (1 <= day <= max_day_in_month[month - 1]):
-            output.append("День указан неверно для указанного месяца.")
+        month, day = inpt
+        if not(month.isdigit()) or not(day.isdigit()):
+            output.append("месяц и день должны быть представлены натуральными числами")
+        else:
+            month, day = map(int, inpt)
+            month,day = int(month),int(day)
+            if not (1 <= month <= 12):
+                output.append("Месяц должен быть от 1 до 12.")
+            else:
+                if not (1 <= day <= max_day_in_month[month - 1]):
+                    output.append("День введён неверно для указанного месяца.")
 
     if output:
         return "Введены недопустимые данные: " + " ".join(output)
@@ -17,5 +24,8 @@ def is_date_correct(day, month):
         return False
 
 
-if not(is_date_correct(day, month)):
+if not(is_date_correct(inpt)):
+    month,day = map(int,inpt)
     print(year_time[(month // 3) % 4])
+else:
+    print(is_date_correct(inpt))
