2e9257dc8a0eae639a3545436c7798fd26ce804c
LOGOPEDEGOL <iegoradamoff@yandex.ru>
2025-12-26 12:59:49 +0300

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,18 @@
+import random
+s = [random.randint(0, 99) for _ in range(10)]
+print(s)
+
+def bubble_sort(s):
+    kolvo_perest = -1
+    while kolvo_perest != 0:
+        kolvo_perest = 0
+
+        for i in range(len(s)-1):
+            if s[i] > s[i+1]:
+                s[i],s[i+1] = s[i+1],s[i]
+                kolvo_perest += 1
+
+    return s
+
+print(bubble_sort(s))
+#ЭТО НЕ ГОТОВЫЙ КОД, ЭТО ОТДЕЛЬНЫЙ КОД ТОЛЬКО С ПРИМЕРОМ РАБОТЫ СОРТИРОВКИ ВЫБОРОМ
