334b4df08c8b61a1a4371c57ccf86ef17b329511
LOGOPEDEGOL <iegoradamoff@yandex.ru>
2025-12-30 23:26:45 +0300

    Create insert_sort.py

---
+++ insert_sort.py
@@ -0,0 +1,17 @@
+import random
+s = [random.randint(0, 99) for _ in range(10)]
+print(s)
+
+def insertion_sort(s):
+    for i in range(1, len(s)):
+        key = s[i]  # Сохраняем значение для вставки
+        j = i - 1
+        while j >= 0 and s[j] > key:  # Сравниваем с key, а не s[i]
+            s[j + 1] = s[j]
+            j -= 1
+        s[j + 1] = key  # Вставляем сохраненное значение
+    return s
+  
+print(selection_sort(s))
+
+#ЭТО НЕ ГОТОВЫЙ КОД, ЭТО ОТДЕЛЬНЫЙ КОД ТОЛЬКО С ПРИМЕРОМ РАБОТЫ СОРТИРОВКИ ВСТАВКАМИ
